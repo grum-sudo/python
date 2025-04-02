97a33ee1f7b48f58b67a1d6adf1aa37c76669d0f
grum-sudo <anzournakhesh@gmail.com>
2025-04-02 23:02:23 +0300

    Update weight_conversion_program.py

--- weight_conversion_program.py
+++ weight_conversion_program.py
@@ -1,1 +1,12 @@
-
+weight = float(input("Enter weight: "))
+unit = input("Enter unit (k/l): ").lower()
+if unit == "k": 
+    weight = weight * 2.205
+    unit = "Lbs."
+    print(f"Weight is: {round(weight, 2)}")
+elif unit == "l":
+    weight = weight / 2.205
+    unit = "Kgs."
+    print(f"Weight is: {round(weight, 2)}")
+else: 
+    print(f"{unit} is not a valid unit")
