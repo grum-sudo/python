3b116683079b4a390b685afaf8eb68b59e55b083
grum-sudo <anzournakhesh@gmail.com>
2025-04-02 22:37:51 +0300

    Add files via upload

---
+++ t.py
@@ -0,0 +1,16 @@
+#typecasting = the process of converting a variable from 1 data type to another
+
+
+name = "grum lol" #str
+age = 22 #int
+gpa = 3.9 #float
+is_student = True #bool
+
+age = str(age)
+
+age += "1"
+
+print(age)
+
+name = bool(name)
+print(name)
