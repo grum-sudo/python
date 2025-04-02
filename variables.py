504b79c8e961c4fc3b200010164a34ea87bc9ebf
grum-sudo <anzournakhesh@gmail.com>
2025-04-02 22:39:56 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,44 @@
+#these are strings
+
+first_name = "anzor"
+food = "pancakes"
+email = "anzourn@proton.me"
+
+print(f"hello {first_name}")
+print (f"you like {food}")
+print(f"your email is {email}")
+
+#integers
+age = 11
+quantity = 15
+num_of_students = 30
+
+print(f"you are {age} years old")
+print(f"you are buying {quantity} eggs")
+print (f"your class has {num_of_students} students")
+
+#float
+price = 3.99
+gpa = 3.9
+distance = 1.2
+
+print(f"these eggs cost ${price}")
+print(f"your gpa is {gpa}")
+print(f"the mall is {distance}km")
+
+#boolean
+is_student = True
+for_sale = False
+is_online = False
+
+print (f"are you a student?: {is_student}")
+
+if for_sale :
+    print("it is for sale")
+else:
+    print("it is not for sale")
+
+if is_online:
+    print("you are online")
+else:
+    print("you are not online")
