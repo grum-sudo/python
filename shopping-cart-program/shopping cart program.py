bc9eb431282d17b433438bbbbf8ac97a86d5fa07
grum-sudo <anzourn@proton.me>
2025-08-02 13:56:39 +0300

    test
    tset

---
+++ shopping-cart-program/shopping cart program.py
@@ -0,0 +1,7 @@
+foods = []
+prices = []
+total = 0
+
+while true:
+    foods = input("enter a food to buy(q to quit): ")
+    if food = (q)
---
+++ test/test.py
@@ -0,0 +1,3 @@
+response = input("what the hell are you doing")
+if response == "nothing":
+    print("ok")
