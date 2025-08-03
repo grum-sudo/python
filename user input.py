02a8fda7af53c844ff450f6ab8b7d4a4b3fe1c4a
grum-sudo <anzourn@proton.me>
2025-08-03 15:45:03 +0300

    removed useless file

--- user input.py
+++
@@ -1,7 +0,0 @@
-
-item = input ("what item would you like to buy?: ")
-price = float(input("what is the price of the item?:"))
-quantity = int(input ("many of the item would you like to buy?:"))
-total = price * quantity
-print(f"you have bought {quantity} x {item}/s")
-print (f"your total is ₺{total}")
