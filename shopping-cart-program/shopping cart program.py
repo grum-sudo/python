eaab808a98f3bace1fa6e9d8dfb606fd74ad43a2
grum-sudo <anzourn@proton.me>
2025-08-29 23:13:25 +0300

    idk some trash

--- shopping-cart-program/shopping cart program.py
+++ shopping-cart-program/shopping cart program.py
@@ -1,7 +1,8 @@
-foods = []
+foods =  []# i dont really understand ts like why [] maybe not {} or ()
 prices = []
 total = 0
 
-while true:
-    foods = input("enter a food to buy(q to quit): ")
-    if food = (q)
+while True:
+    food = input("enter what you want buy: (q quit)")
+    if food.lower() == "q":
+        break
