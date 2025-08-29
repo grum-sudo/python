b748c47e788741bcaa5460adc2847e5801a742eb
grum-sudo <anzourn@proton.me>
2025-08-29 23:23:53 +0300

    shopping thingy kinda cool

--- shopping-cart-program/shopping cart program.py
+++ shopping-cart-program/shopping cart program.py
@@ -1,4 +1,4 @@
-foods =  []# i dont really understand ts like why [] maybe not {} or ()
+foods =  []# i dont really understand ts like why [] and not {} or ()
 prices = []
 total = 0
 
@@ -6,3 +6,17 @@ while True:
     food = input("enter what you want buy: (q quit)")
     if food.lower() == "q":
         break
+    else:
+        price = float(input("enter the price: "))
+        foods.append(food)
+        prices.append(price)
+
+print("---- YOUR CART ----")
+
+for food in foods:
+    print (food)
+
+for price in prices:
+    total += price
+
+print(f"you total is ${total}")#the python tut said to add empty print but i dont wanna and it doesnt even matter
