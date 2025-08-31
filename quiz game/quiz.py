6c51c52914875a5beb03be1c308cee66ccd0dbc4
grum-sudo <anzourn@proton.me>
2025-08-31 12:58:29 +0300

    shit am crashing out on

---
+++ quiz game/quiz.py
@@ -0,0 +1,13 @@
+questions = ("how many elements are in the periodic table?: ",
+             "which animal lays the largest eggs?: ",)
+
+options = (("a. ",),
+           (),
+           (),
+           (),
+           ())
+
+answers = ()
+guesses = []
+score = 0
+question_num = 0
