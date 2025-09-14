14d64ee64357fc5bc927140bb749c14d4a0b6cbb
grum-sudo <anzournakhesh@gmail.com>
2025-09-14 19:54:21 +0300

    ts tuff

--- random numbers/number.py
+++ random numbers/number.py
@@ -7,7 +7,7 @@ cards = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" 
 
 # number = random.randint(low, high)
 # number = random.random()
-# option = random.choice(options)
+option = random.choice(options)
 random.shuffle(cards)
 
 print (option)
