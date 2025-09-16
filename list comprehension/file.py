f17158e1927529b3a1769e6bed426e80949ba052
grum-sudo <anzournakhesh@gmail.com>
2025-09-16 18:49:32 +0300

    sdfsdf

---
+++ list comprehension/file.py
@@ -0,0 +1,11 @@
+#doubles = [x * 2 for x in range(1, 11)]
+#triples = [x * 3 for x in range(1, 11)]
+#square = [z * z for z in range(1, 11)] 
+#print(doubles)
+#print(triples)
+#print(square)
+
+fruits = ["apple", "orange", "banana", "coconut"]
+
+fruits = [fruit.upper() for fruit in fruits]
+print(fruits)
