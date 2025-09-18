29c1d0e829bfd04b1edbeb2db1260f83a681f9cc
grum-sudo <anzournakhesh@gmail.com>
2025-09-18 16:26:20 +0300

    hhhh

--- list comprehension/file.py
+++ list comprehension/file.py
@@ -5,7 +5,16 @@
 #print(triples)
 #print(square)
 
-fruits = ["apple", "orange", "banana", "coconut"]
+#fruits = ["apple", "orange", "banana", "coconut"]
+#fruit_chars = [fruit[0] for fruit in fruits]
+#print(fruit_chars)
 
-fruits = [fruit.upper() for fruit in fruits]
-print(fruits)
+#numbers = [1, -2, 3, -4, 5, -6]
+#positive_nums = [num for num in numbers if num >= 0]
+#negative_nums = [num for num in numbers if num <= 0]
+#even_nums = [num for num in numbers if num % 2 == 0]
+#odd_nums = [num for num in numbers if num % 2 == 1]
+#print(even_nums)
+grades = [85, 42, 79, 90, 56, 61, 30]
+
+passing_grade = [grade for grade in grades if grade >= ]
